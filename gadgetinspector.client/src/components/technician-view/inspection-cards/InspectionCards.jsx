fcef74f5e690e9571b26b1361f26d7095706c41a
Kevin Seymour <kevin.o.seymour@gmail.com>
2025-03-24 21:25:05 -0400

    Add a display to Technician View when there are no cards to show

--- gadgetinspector.client/src/components/technician-view/inspection-cards/InspectionCards.jsx
+++ gadgetinspector.client/src/components/technician-view/inspection-cards/InspectionCards.jsx
@@ -1,4 +1,4 @@
-import { Spinner } from "reactstrap";
+import { Spinner, Alert } from "reactstrap";
 import InspectionCard from "./inspection-card/InspectionCard";
 import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
@@ -33,9 +33,19 @@ export default function InspectionCards(props) {
         setInspections(inspectionsWithItemRemoved);
     }, [inspections]);
 
-    if (!selectedTechnicianId) return (<></>);
+    if (!selectedTechnicianId) return (
+        <></>
+    );
 
-    if (isLoading) return (<h4 style={{textAlign: "center"}} className="mt-5"><Spinner></Spinner> Loading...</h4>);
+    if (isLoading) return (
+        <h4 style={{ textAlign: "center" }} className="mt-5"><Spinner></Spinner> Loading...</h4>
+    );
+
+    if (!inspections.length) return (
+        <div style={{width: "100%"}} className="d-flex">
+            <Alert color="primary" className="mt-5 margin-auto" style={{ width: "fit-content" }}>You have no inspections currently scheduled.</Alert>
+        </div>
+    );
 
     return (
         <div className="d-flex flex-wrap">
